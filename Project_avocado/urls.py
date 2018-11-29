359afaa409557cc8f16388ead41a9d79d40dac94
Mauricio Collazos <ma0@contraslash.com>
2018-11-28 22:24:23 -0500

    solved issue with namespace

--- Project_avocado/urls.py
+++ Project_avocado/urls.py
@@ -18,5 +18,10 @@ from django.urls import (path, include)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('aplicaciones/', include(("applications.agregar_proyectos.urls", "agregar_proyectos")))
+    path(
+        'aplicaciones/',
+        include(
+            "applications.agregar_proyectos.urls",
+        )
+    )
 ]
